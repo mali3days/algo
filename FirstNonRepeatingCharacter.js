3c6f05b63861efeadb548813117e29be2d37346b
Bugay Nikolay <nikolay.bugay@nixsolutions.com>
2022-04-21 20:53:10 +0300

    FirstNonRepeatingCharacter solution2 is added

--- FirstNonRepeatingCharacter.js
+++ FirstNonRepeatingCharacter.js
@@ -7,7 +7,7 @@
  *
  */
 
-// Solution
+// Solution 1
 function firstNonRepeatingCharacter(string) {
 	for (let i = 0; i < string.length; i++) {
 		if (string.indexOf(string[i]) === string.lastIndexOf(string[i])) {
@@ -18,4 +18,27 @@ function firstNonRepeatingCharacter(string) {
   return -1;
 }
 
-isPalindrome('abcdcaf'); // 1
+// Solution 2
+function firstNonRepeatingCharacter(string) {
+	let firstNonRepeatingChar = -1;
+	
+	for (let i = 0; i < string.length; i++) {
+			for (let j = 0; j < string.length; j++) {
+				if (i !== j && string[i] === string[j]) {
+					firstNonRepeatingChar = -1;
+					break;
+				} else {
+					firstNonRepeatingChar = i;
+				}
+			}
+		
+		  if (firstNonRepeatingChar > -1) {
+				return firstNonRepeatingChar;
+			}
+	}
+
+  return firstNonRepeatingChar;
+}
+
+
+firstNonRepeatingCharacter('abcdcaf'); // 1
