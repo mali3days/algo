486d6833bd2bf40fff510229d7c36cfd5beda7da
mali3days <mali3days@gmail.com>
2022-05-10 21:56:46 +0300

    update solution for InsertionSort and add time&space complexity

--- InsertionSort.js
+++ InsertionSort.js
@@ -7,21 +7,26 @@
  */
 
 // Solution 1
+// Best:      O(n) time | O(1) space
+// Average: O(n^2) time | O(1) space
+// Worst:   O(n^2) time | O(1) space
 function insertionSort(array) {
   for (let i = 1; i < array.length; i++) {
-    for (let j = i - 1; j > -1; j--) {
-      if (array[i] < array[j]) {
-        const buff = array[j];
-
-        array[j] = array[i];
-        array[i] = buff;
-
-        i--;
-      }
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      swap(j, j - 1, array);
+      j -= 1;
     }
   }
 
   return array;
+}
+
+function swap(i, j, array) {
+  const buff = array[j];
+
+  array[j] = array[i];
+  array[i] = buff;
 }
 
 insertionSort([8, 5, 2, 9, 5, 6, 3]); // [2,3,5,5,6,8,9]
