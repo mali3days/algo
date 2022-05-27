7f6dfb6ab127ff26500e3e988e8f78e9b4f5cc1f
mali3days <mali3days@gmail.com>
2022-05-27 21:44:39 +0300

    add solution2 for PalindromeCheck

--- PalindromeCheck.js
+++ PalindromeCheck.js
@@ -7,8 +7,9 @@
  *
  */
 
-// Solution
-function isPalindrome(string) {
+// Solution 1
+// O(n) time | O(1) space
+function isPalindrome1(string) {
   let l = 0;
   let r = string.length - 1;
   while (l < r) {
@@ -19,4 +20,15 @@ function isPalindrome(string) {
   return true;
 }
 
-isPalindrome('abcdcba'); // true
+// Solution 2
+// O(n) time | O(1) space
+function isPalindrome2(string) {
+  for (let i in string) {
+    if (string[i] !== string[string.length - 1 - i]) return false;
+  }
+
+  return true;
+}
+
+isPalindrome1('abcdcba'); // true
+isPalindrome2('abcdcba'); // true
