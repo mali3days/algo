6f4222c7eb77294842901fcc27ba1446e07a1d4e
mali3days <mali3days@gmail.com>
2022-10-24 23:45:18 +0300

    add naive solution for ArrayOfProducts with O(n) time complexity

--- ArrayOfProducts.js
+++ ArrayOfProducts.js
@@ -15,4 +15,24 @@ function arrayOfProducts1(array) {
   return result;
 }
 
+function arrayOfProducts2(array) {
+  let result = new Array(array.length).fill(1);
+  let product = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    result[i] = product;
+    product = product * array[i];
+  }
+
+  product = 1;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    result[i] = product * result[i];
+    product = product * array[i];
+  }
+
+  return result;
+}
+
 console.log(arrayOfProducts1([5, 1, 4, 2])); // [8, 40, 10, 20]
+console.log(arrayOfProducts2([5, 1, 4, 2])); // [8, 40, 10, 20]
